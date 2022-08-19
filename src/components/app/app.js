d2dbbab0da3a06363db4914ebefdfa623cc53275
Alex Vedernikov <100806495+AlexVedernikoff@users.noreply.github.com>
2022-08-19 04:50:26 +0300

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -172,7 +172,7 @@ export default class App extends Component {
           newItem,
           ...todoData.slice(idx + 1)
         ];
-        //localStorage.setItem("todoData", JSON.stringify(newArray));
+        localStorage.setItem("todoData", JSON.stringify(newArray));
         return {
           todoData: newArray
         };
@@ -226,7 +226,7 @@ export default class App extends Component {
       return this.newData;
     } else {
       this.newData = this.state.todoData;
-      //localStorage.setItem("todoData", JSON.stringify(this.state.todoData));
+      localStorage.setItem("todoData", JSON.stringify(this.state.todoData));
       return this.state.todoData;
     }
   }
